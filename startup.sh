0633369a1d5f3ec76c6db802efabade4ee27a269
kuzan04 <kittiphum1230@hotmail.co.th>
2022-09-25 15:08:34 +0700

    Modify startup.

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 PATH="$( cd -- "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
 `source $PATH/env/bin/activate`
-`sudo python3 $PATH/server.py`
+#`sudo python3 $PATH/server.py`
+`pm2 start $PATH/server.py --name agentListen --interpretor python3`
