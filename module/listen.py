f00de8462dc88459dcb22ee595f5564a6e92ec0e
kuzan04 <kittiphum1230@hotmail.co.th>
2022-10-19 22:28:13 +0700

    Fix error query update.

--- module/listen.py
+++ module/listen.py
@@ -104,7 +104,7 @@ class SSLServer:
                         cur_manage.execute(f"INSERT INTO {table_history} ({column_history}) VALUE ({selected[0]})")
                         self._connect.commit()
                     else:
-                        cur_manage.execute(f"UPDATE FROM {table_history} SET _get_ = NOW() WHERE agm_id = {find_history[idx]};")
+                        cur_manage.execute(f"UPDATE {table_history} SET _get_ = NOW() WHERE agm_id = {find_history[idx]};")
                         self._connect.commit()
                 elif "!CHECK" in msg:
                     print("Ok")
