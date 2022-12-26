8cd15541b37a507966864a0b1ef7c6cd16ceebe8
kuzan04 <kittiphum1230@hotmail.co.th>
2022-12-26 16:06:24 +0700

    no comment.

--- module/db.py
+++ module/db.py
@@ -185,7 +185,7 @@ class DBCheck:
             elif old[mark][i] != val[i] and i != 0:
                 print(column)
             else:
-                print(old[mark][i], val[i])
+                print(old[mark], val[i])
                 return self.update(old, mark, column, (i+1))
         except IndexError:
             print('indexError')
