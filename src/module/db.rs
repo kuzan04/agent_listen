0e0eff325cc5bdf46d0c2837a49b7794835120ac
kuzan04 <kittiphum1230@hotmail.co.th>
2023-05-31 23:59:45 +0700

    Oracle can get tables after query one more time, Err(InvalidOperation(Could not use the 'query' method for non-select statements))

--- src/module/db.rs
+++ src/module/db.rs
@@ -36,9 +36,9 @@ impl TestConnect {
                     .fetch_all(&pool)
                     .await.unwrap()
                     .into_iter()
-                    .map(|row| match row.try_get(format!("Tables_in_{}", self.database).as_str()) {
+                    .map(|row| match row.try_get(format!("Tables_in_{}", self.database.to_lowercase()).as_str()) {
                         Ok(row) => row,
-                        Err(_) => row.get(format!("Tables_in_{}", self.database.to_lowercase()).as_str()),
+                        Err(_) => row.get(format!("Tables_in_{}", self.database).as_str()),
                     })
                     .collect();
                 let mut mix: Vec<Table> = vec![];
@@ -58,23 +58,37 @@ impl TestConnect {
         }
     }
 
-    pub async fn oracle(&self) -> Result<String, oracle::Error> {
+    async fn oracle_query(&self, query: &str) -> Result<Vec<String>, oracle::Error> {
         //Set Oracle instant client.
         // std::env::set_var("LD_LIBRARY_PATH", "/Users/kuzan04/Desktop/instantclient_19_8/");
         let pool = PoolBuilder::new(self.user.as_str(), self.passwd.as_str(), format!("//{}:1521/{}", self.host, self.database).as_str())
-            .max_connections(5)
+            .max_connections(10)
             .build();
-        match pool {
+        let result: Vec<String> = match pool {
             Ok(conn) => {
-                match conn.get() {
-                    Ok(conn) => {
-                        println!("{:?}", conn);
-                        Ok("Hello".to_string())
-                    },
-                    Err(e) => Ok(e.to_string())
-                }
+                let connection = conn.get()?;
+                let res: Vec<String> = connection.query(query, &[])?
+                    .into_iter()
+                    .map(|row| match row.expect("REASON").get(0) {
+                        Ok(row) => row,
+                        Err(e) => e.to_string(),
+                    })
+                    .collect();
+                connection.close()?;
+                res
             },
-            Err(e) => Ok(e.to_string())
+            Err(e) => vec![e.to_string()]
+        };
+        Ok(result)
+    }
+
+    pub async fn oracle(&self) -> Result<String, oracle::Error> {
+        let tables = self.oracle_query("SELECT table_name FROM user_tables").await?;
+        // let mut columns: Vec<Vec<String>>;
+        for i in tables {
+            let column = self.oracle_query(format!("DESC {}", i).as_str()).await;
+            println!("{:?}", column);
         }
+        Ok("Hello".to_string())
     }
 }
