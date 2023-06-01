e74d22d0464cdec1c9d69edcb6260669c377342c
kuzan04 <kittiphum1230@hotmail.co.th>
2023-06-01 16:42:16 +0700

    Test new fn query_column.

--- src/module/db.rs
+++ src/module/db.rs
@@ -58,7 +58,7 @@ impl TestConnect {
         }
     }
 
-    async fn oracle_query(&self, query: &str) -> Result<Vec<String>, oracle::Error> {
+    async fn oracle_query_table(&self, query: &str) -> Result<Vec<String>, oracle::Error> {
         //Set Oracle instant client.
         // std::env::set_var("LD_LIBRARY_PATH", "/Users/kuzan04/Desktop/instantclient_19_8/");
         let pool = PoolBuilder::new(self.user.as_str(), self.passwd.as_str(), format!("//{}:1521/{}", self.host, self.database).as_str())
@@ -82,11 +82,33 @@ impl TestConnect {
         Ok(result)
     }
 
+    async fn oracle_query_column(&self, query: &str) -> Result<Vec<String>, oracle::Error> {
+        let pool = PoolBuilder::new(self.user.as_str(), self.passwd.as_str(), format!("//{}:1521/{}", self.host, self.database).as_str())
+            .max_connections(10)
+            .build();
+        let result: Vec<String> = match pool {
+            Ok(conn) => {
+                let connection = conn.get()?;
+                let res: Vec<String> = connection.query(query, &[])?
+                    .into_iter()
+                    .map(|row| match row.expect("REASON").get(0) {
+                        Ok(row) => row,
+                        Err(e) => e.to_string(),
+                    })
+                    .collect();
+                connection.close()?;
+                res
+            },
+            Err(e) => vec![e.to_string()]
+        };
+        Ok(result)
+    }
+
     pub async fn oracle(&self) -> Result<String, oracle::Error> {
-        let tables = self.oracle_query("SELECT table_name FROM user_tables").await?;
+        let tables = self.oracle_query_table("SELECT table_name FROM user_tables").await?;
         // let mut columns: Vec<Vec<String>>;
         for i in tables {
-            let column = self.oracle_query(format!("DESC {}", i).as_str()).await;
+            let column = self.oracle_query_column(format!("DESC {}", i).as_str()).await;
             println!("{:?}", column);
         }
         Ok("Hello".to_string())
