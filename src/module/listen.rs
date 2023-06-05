82ee4d74522765a326b396930ffee76c3e7faa96
kuzan04 <kittiphum1230@hotmail.co.th>
2023-06-05 23:21:25 +0700

    Test connection client log0 hash close to success (1/3).

--- src/module/listen.rs
+++ src/module/listen.rs
@@ -1,7 +1,6 @@
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net;
 use sqlx::{MySqlPool, FromRow};
-
 use crate::module::{
     log0::LogHash,
     file::FileDirectory,
@@ -88,7 +87,7 @@ impl Recevie {
         }
     }
 
-    async fn set_history(db: MySqlPool, manager: Vec<AgentManage>, code: String, name: String) { //  -> String
+    async fn set_history(db: MySqlPool, manager: Vec<AgentManage>, code: String, name: String) -> String {  
         let env_history = Self::split_string(&dotenv::var("TB_HISTORY").unwrap_or_else(|_| "TB_TR_PDPA_AGENT_LISTEN_HISTORY:agm_id".to_string()), ":");
         let history: Vec<AgentHistory> = sqlx::query(
             format!(
@@ -105,7 +104,7 @@ impl Recevie {
         let selected = Self::set_manage(manager, code, name);
         match selected {
             Ok(agm) => {
-                let message = String::new(); //mut
+                let mut message = String::new(); //mut
                 let mut i = 0;
                 while i < history.len() {
                     if history[i].agm_id == agm.agm_id {
@@ -113,7 +112,7 @@ impl Recevie {
                             .bind(agm.agm_id)
                             .execute(&db)
                             .await.unwrap();
-                        // message = "Success".to_string()
+                        message = "Success".to_string()
                     }
                     i += 1
                 }
@@ -123,13 +122,12 @@ impl Recevie {
                             .bind(agm.agm_id)
                             .execute(&db)
                             .await.unwrap();
-                        // "Success".to_string()
+                        "Success".to_string()
                     }
-                    _ => todo!(), //"Success".to_string()
+                    _ => "Success".to_string()
                 }
             },
-            Err(_) => todo!()
-                //format!("[Error] {} agent client from web alltra", err)
+            Err(err) => format!("[Error] {} agent client from web alltra", err)
         }
     }
 
@@ -239,10 +237,10 @@ impl Recevie {
                         // Get AG_NAME after success.
                         let result = Self::main_task(Self::status_store(store, response[0].clone()), response.clone(), db.clone()).await;
                         // Set message to response client.
-                        Self::set_history(db.clone(), manager, response[0].to_owned(), result).await;
-                        // if let Err(error) = stream.write_all(message.as_bytes()).await {
-                        //     println!("Failed to write to stream: {}", error);
-                        // }
+                        message = Self::set_history(db.clone(), manager, response[0].to_owned(), result).await;
+                        if let Err(error) = stream.write_all(message.as_bytes()).await {
+                            println!("Failed to write to stream: {}", error);
+                        }
                     },
                     _ => {
                         message = "Failed unknow type agent. Please check token, .env, or api from alltra again!!".to_string();
@@ -271,9 +269,10 @@ impl Recevie {
                 tokio::spawn(async move {
                     Self::handle_client(sock.0, cloned_db).await;
                 });
-            } else {
-                println!("Failed accepting connection!");
-            }
+            } 
+            // else {
+            //     println!("Failed accepting connection!");
+            // }
         }
     }
 }
--- src/module/log0.rs
+++ src/module/log0.rs
@@ -12,13 +12,39 @@ impl LogHash {
         LogHash { connection, table, column, content }
     }
 
+    // Helper client rerun.
+    async fn check(&mut self) -> bool {
+        let value_where = self.content
+            .iter()
+            .zip(self.column.iter())
+            .map(|(x, y)| format!("{}=\"{}\"", y.trim(), x.trim()))
+            .collect::<Vec<String>>();
+        let mut query2 = String::new();
+        for i in 0..value_where.len() {
+            if i == value_where.len() - 1 {
+                query2.push_str(&value_where[i]);
+            } else {
+                query2.push_str(format!("{} AND ", value_where[i]).as_str());
+            }
+        }
+        let query = "SELECT id FROM (SELECT * FROM TB_TR_PDPA_AGENT_LOG0_HASH ORDER BY id DESC) as log0";
+        sqlx::query(format!("{} WHERE {} LIMIT 1", query, query2).as_str())
+            .fetch_one(&self.connection)
+            .await.is_ok()
+    }
+
     pub async fn build(&mut self) -> Result<String, Box<dyn std::error::Error>> {
         let name = self.content.remove(0);
-        let values = self.content.iter().map(|s| format!("\"{}\"", s)).collect::<Vec<String>>().join(",");
-        let query = format!("INSERT INTO {} ({}) VALUES ({})", self.table, self.column.join(","), values);
-        sqlx::query(&query)
-            .execute(&self.connection)
-            .await?;
-        Ok(name)
+        match self.check().await {
+            true => Ok(name),
+            false => {
+                let values = self.content.iter().map(|s| format!("\"{}\"", s)).collect::<Vec<String>>().join(",");
+                let query = format!("INSERT INTO {} ({}) VALUES ({})", self.table, self.column.join(","), values);
+                sqlx::query(&query)
+                    .execute(&self.connection)
+                    .await?;
+                Ok(name)
+            }
+        }
     }
 }
